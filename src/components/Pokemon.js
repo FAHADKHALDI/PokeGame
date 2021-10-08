0cb1aadd45aad1c37f815cac3a1fea4f2c59d45a
FAHADKHALDI <fahadalkhaldi96@gmail.com>
2021-10-09 02:05:32 +0300

    Done :)

---
+++ src/components/Pokemon.js
@@ -0,0 +1,59 @@
+import src1 from "./images/pikachu.png";
+import src2 from "./images/Pokemon1.png";
+import src3 from "./images/Pokemon2.png";
+import src4 from "./images/Pokemon3.png";
+import src5 from "./images/Pokemon4.png";
+import src6 from "./images/Pokemon5.png";
+
+const images = [
+  {
+    src: src1,
+    player: "pikachu",
+  },
+  {
+    src: src1,
+    player: "pikachu",
+  },
+  {
+    src: src2,
+    player: "Pokemon1",
+  },
+  {
+    src: src2,
+    player: "Pokemon1",
+  },
+  {
+    src: src3,
+    player: "Pokemon2",
+  },
+  {
+    src: src3,
+    player: "Pokemon2",
+  },
+  {
+    src: src4,
+    player: "Pokemon3",
+  },
+  {
+    src: src4,
+    player: "Pokemon3",
+  },
+  {
+    src: src5,
+    player: "Pokemon4",
+  },
+  {
+    src: src5,
+    player: "Pokemon4",
+  },
+  {
+    src: src6,
+    player: "Pokemon5",
+  },
+  {
+    src: src6,
+    player: "Pokemon5",
+  },
+];
+
+export { images };
